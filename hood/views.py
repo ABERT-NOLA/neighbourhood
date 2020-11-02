469cadd0135030d77494bc351255f346045563ea
joan evans <nyambegaobwoge@yahoo.com>
2020-11-02 11:55:07 +0300

    implemented add hood for admins

--- hood/views.py
+++ hood/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from users.models import User, Member, HoodAdmin
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
 from users.decorators import member_required
+from .models import Business, Neighbourhood, Post
 # Create your views here.
 
 def index(request):
@@ -31,9 +32,87 @@ class BusinessCreateView(UserPassesTestMixin, CreateView):
     model = Business
     fields = ['name', 'email', 'desc', 'neighbourhood']
 
-    
+    def test_func(self):
+        return True  
+
     def form_valid(self, form):
         print('great')
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+@method_decorator(login_required, name='dispatch')
+class BusinessUpdateView(UserPassesTestMixin, UpdateView):
+    model = Business
+    fields = ['name', 'email', 'desc', 'neighbourhood']
+
+    def test_func(self):
+        return True  
+
+    def form_valid(self, form):
+        print('great')
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class BusinessDetailView(DetailView):
+    model = Business
+
+@method_decorator(login_required, name='dispatch')
+class BusinessDeleteView(UserPassesTestMixin, DeleteView):
+    model = Business
+    fields = ['name', 'email', 'desc', 'neighbourhood']
+    success_url = '/'
+
+   
+@method_decorator(login_required, name='dispatch')
+class HoodCreateView(UserPassesTestMixin, CreateView):
+    model = Neighbourhood
+    fields = '__all__'      
+
+    def test_func(self):
+        return True    
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class HoodDetailView(DetailView):
+    model = Neighbourhood
+
+@method_decorator(login_required, name='dispatch')
+class HoodUpdateView(UserPassesTestMixin, UpdateView):
+    model = Neighbourhood
+    fields = '__all__'
+               
+    def test_func(self):
+        return True  
+        
+    def form_valid(self, form):
+        print('great')
+        return super().form_valid(form)
+
+@method_decorator(login_required, name='dispatch')
+class HoodDeleteView(UserPassesTestMixin, DeleteView):
+    model = Neighbourhood
+    fields = '__all__'
+    success_url = '/'
+             
+
+    def form_valid(self, form):
+        print('great')
+        return super().form_valid(form)
+
+@method_decorator(login_required, name='dispatch')
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content', 'image']
+
+    def form_valid(self, form):
+        print('great')
+        print(self.request.user.userprofile.neighbourhood)
+        form.instance.neighbourhood = self.request.user.userprofile.neighbourhood
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class PostDetailView(DetailView):
+    model = Post
