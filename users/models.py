7a15614ed6e00df38dc1ff9f4f7d9615354c9f1f
joan evans <nyambegaobwoge@yahoo.com>
2020-11-03 10:48:24 +0300

    added a neighbourhood FK

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from PIL import Image
 
-
 # Create your models here.
 class User(AbstractUser):
     is_hood_admin = models.BooleanField(default=False)
