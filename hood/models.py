9aec62c136bc29d7d7b49cd39fd4de36d6431baa
joan evans <nyambegaobwoge@yahoo.com>
2020-11-03 10:49:27 +0300

    Added a comment model

--- hood/models.py
+++ hood/models.py
@@ -17,7 +17,7 @@ class Neighbourhood(models.Model):
         return f'{self.name}'
 
     def get_absolute_url(self):
-        return reverse('welcome') 
+        return reverse('admin_pr') 
 
     @classmethod
     def get_all_neighbourhoods(cls):
@@ -36,7 +36,7 @@ class Business(models.Model):
         return f'{self.name}'
 
     def get_absolute_url(self):
-        return reverse('welcome') 
+        return reverse('admin_pr') 
 
     @classmethod
     def get_all_businesses(cls):
@@ -48,14 +48,22 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_pics', default='default.jpg')
     date_posted = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.title}'
 
     def get_absolute_url(self):
-        return reverse('welcome') 
+        return reverse('welcome-index') 
 
     @classmethod
     def get_all_posts(cls):
         return cls.objects.order_by('-date_posted')
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.content
